3f032615765d82f0f16451142717ccf60c9a766c
zux <zux.grr@gmail.com>
2024-12-06 09:23:00 +0500

    Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -0,0 +1,39 @@
+package ru.job4j.grabber.model;
+
+import java.util.Objects;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String link;
+    private String description;
+    private Long time;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Post post)) {
+            return false;
+        }
+        return Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, link, description);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Post{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", link='").append(link).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append(", time=").append(time);
+        sb.append('}');
+        return sb.toString();
+    }
+}
