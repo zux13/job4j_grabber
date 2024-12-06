5003bf189d957d0f9b38b915a7e8cd799f24e436
zux <zux.grr@gmail.com>
2024-12-06 10:11:38 +0500

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,49 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(Integer.parseInt(getConfig().getProperty("rabbit.interval")))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    private static Properties getConfig() {
+        Properties config = new Properties();
+        try (InputStream input = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            config.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return config;
+    }
+}
